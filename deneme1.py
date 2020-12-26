f24115693bdb061e51d77a9864394c01d61e0050
Glayk <gulayik@gmail.com>
2020-12-26 14:12:10 +0300

    update

--- deneme1.py
+++ deneme1.py
@@ -1,11 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 28 16:39:15 2020
-
-@author: Gul_a
-"""
-
-import numpy as np 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -54,7 +47,7 @@ plt.xlabel('Kategoriler', fontsize = 15)
 plt.ylabel('Toplam', fontsize = 15);
 #etiketlerin görselleştirilmesi - pasta grafiği
 fig, ax = plt.subplots(figsize=(15, 10))
-ax.pie(df.Toplam, labels =df.duygu, autopct = '%1.2f%%',  startangle = 90 )
+ax.pie(df.Toplam, labels =df.duygu, autopct = '%1.2f%%', startangle = 90 )
 ax.axis('equal')
 """
 
@@ -70,53 +63,63 @@ def remove_stopwords(veri):
 tweet['stop_veri'] = tweet['veri'].apply(lambda veri: remove_stopwords(veri))
 
 """
-def stemming_tokenizer(veri): 
-    stemmer = TurkishStemmer()
-    return [stemmer.stemWord(w) for w in word_tokenize(veri)]
+def stemming_tokenizer(veri):
+stemmer = TurkishStemmer()
+return [stemmer.stemWord(w) for w in word_tokenize(veri)]
 tweet['token_veri'] = tweet['stop_veri'].apply(lambda veri: stemming_tokenizer(veri))
 print(tweet['token_veri'].head(20))
 """
 
 #a=tweet.loc[tweet.duygu == "pozitif" ]
-a=tweet.loc[tweet.duygu.isin(['pozitif', 'negatif'])]
+tag_tweet=tweet.loc[tweet.duygu.isin(['pozitif', 'negatif'])]
+tweet_datas = tag_tweet['stop_veri'].values.tolist()
+tweet_sentiment= tag_tweet['duygu'].values.tolist()
+tweet_all = tweet['stop_veri'].values.tolist()
 
-
-tweetDoc = a['stop_veri'].values.tolist()
-tweetClass = a['duygu'].values.tolist()
-
-
-#test ve train olarak ayırma 
-x_train, x_test, y_train, y_test = train_test_split(tweetDoc, tweetClass, test_size = 0.25, random_state = 42)
+#test ve train olarak ayırma
+x_train, x_test, y_train, y_test = train_test_split(tweet_datas, tweet_sentiment, test_size = 0.25, random_state = 42)
 print('The length of the trainning set are {}'.format(len(x_train)))
 print('The length of the test set are {}'.format(len(x_test)))
 
-#tfidf 
+#tfidf
 tfidf_vectorizer = TfidfVectorizer(ngram_range=(1,2), max_df=0.9, min_df = 5)
 x_train_tfidf = tfidf_vectorizer.fit_transform(x_train)
 x_test_tfidf = tfidf_vectorizer.transform(x_test)
+tweet_all_tfidf = tfidf_vectorizer.transform(tweet_all)
 
+#logistic regresyon
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 
-#logistic regresyon
 model = LogisticRegression()
 model.fit(x_train_tfidf, y_train)
 
 y_pred = model.predict(x_test_tfidf)
-print ("Accuracy={}".format(accuracy_score(y_test, y_pred)))
+
+tweet_all_pred = model.predict(tweet_all_tfidf)
+print ("Accuracy={}".format(accuracy_score(y_test, tweet_all_pred[:200])))
 logisticpred = accuracy_score(y_test, y_pred)
 
 
+
+
+
 """
-from sklearn.metrics import confusion_matrix  
-cm = confusion_matrix(y_test, y_pred)
+from collections import Counter
+list = tweet_all_pred.ravel()
+c= dict(Counter(list))
+print(c)
+"""
+"""
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, b_pred[:200])
 sns.heatmap(cm, annot=True, fmt=".0f")
 plt.xlabel('Predicted Values')
 plt.ylabel('Actual Values')
 plt.title('Accuracy Score: {0}'.format(accuracy_score), size = 15)
 plt.show()
-
 """
+
 """
 print(model.score(x_train_tfidf, y_train))
 print(model.score(x_test_tfidf, y_test))
@@ -124,8 +127,3 @@ print(model.score(x_test_tfidf, y_test))
 from sklearn.metrics import classification_report
 print(classification_report(y_test, y_pred))
 """
-
-
-
-
-        
